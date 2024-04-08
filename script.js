383ba997099660a37511e12fa275658f195ec773
ShchebyvovkMax <141161593+ShchebyvovkMax@users.noreply.github.com>
2024-04-08 16:46:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var button = document.getElementsByTagName("button")[2];
-button.addEventListener("click", function(){
-	console.log("Click");
-})
+// var button = document.getElementsByTagName("button")[2];
+// button.addEventListener("click", function(){
+// 	console.log("Click");
+// })
+var id = 'victory';
