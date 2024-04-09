492387672cf4a386898818bfbf1798ed7195250f
ShchebyvovkMax <141161593+ShchebyvovkMax@users.noreply.github.com>
2024-04-09 14:33:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
 // 	console.log("Click");
 // })
 var id = 'victory';
+lpTag.identities=[];
+lpTag.identities.push(identityFn);
+function identityFn(callback) {
+	callback({
+		iss: “ replace with issuer ”,
+		acr: “loa1”,
+		sub: “ replace with customerID ”
+	});
+}
